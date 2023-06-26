4bf7fc2d8263eed2cb7787db3e5bd12048874210
NodirbekDavlatboyev <136903132+NodirbekDavlatboyev@users.noreply.github.com>
2023-06-27 00:47:18 +0500

    lesson_(5.1).py
    yangi python kodlari qo'shildi

---
+++ lesson_(5.1).py
@@ -0,0 +1,59 @@
+# =============================================================================
+# inson = {
+#         'ismi':'Anvar',
+#         'tug\'ilgan_yili':'1986',
+#         'shahri':'Samarqand
+#         }
+# print(otam)
+# =============================================================================
+
+# =============================================================================
+# sevimli_taom = {
+#                'Hasan':'Manti',
+#                'Sharofat':'Osh',
+#                'Nilufar':'Somsa',
+#                'Nodirbek':'Sho\'rva',
+#                'Nigora':'Baliq'
+#                }
+# print(sevimli_taom['Nigora'])
+# print(f"Mening eng yaxshi ko'rgan taomim: {sevimli_taom['Nodirbek']}")
+# print(sevimli_taom['Hasan'])
+# =============================================================================
+
+# =============================================================================
+# python = {
+#          'integer':'butun son',
+#          'float':'haqiqiy son',
+#          'string':'matn',
+#          'if':'shartlarni tekshirish operatori',
+#          'else':'aks holda shart operatori',
+#          'for':'For tsikli ketma-ketlikni takrorlash uchun ishlatiladi',
+#          'list':'ro\'yhat',
+#          'tuple':'o\'zgarmas ro\'yhat',
+#          'print':'malum xabarni ekranga chiqaradi',
+#          'boolean':'mantiqiy qiymat'
+#          }
+# print(python)
+# =============================================================================
+
+# =============================================================================
+# python_words= {
+#          'integer':'butun son',
+#          'float':'haqiqiy son',
+#          'string':'matn',
+#          'if':'shartlarni tekshirish operatori',
+#          'else':'aks holda shart operatori',
+#          'for':'For tsikli ketma-ketlikni takrorlash uchun ishlatiladi',
+#          'list':'ro\'yhat',
+#          'tuple':'o\'zgarmas ro\'yhat',
+#          'print':'malum xabarni ekranga chiqaradi',
+#          'boolean':'mantiqiy qiymat'
+#          }
+# 
+# word = input("ixtiyoriy so'z kiriting: ").lower()
+# qiymat = python_words.get(word)
+# if qiymat == None:
+#     print("Kechirasiz, bizda bu haqida ma'lumot yo'q")
+# else:
+#     print(f"{word} - {qiymat}")
+# =============================================================================
