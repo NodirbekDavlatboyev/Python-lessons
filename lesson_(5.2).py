58eb3066372fad34bd559b655ff9996ed1d25167
NodirbekDavlatboyev <136903132+NodirbekDavlatboyev@users.noreply.github.com>
2023-07-01 00:52:28 +0500

    python lessons
    new practice codes

---
+++ lesson_(5.2).py
@@ -0,0 +1,82 @@
+# =============================================================================
+# Python = {
+#          'apple':'olma',
+#          'banana':'banan',
+#          'pineapple':'ananas',
+#          'pear':'nok',
+#          'pomegranate':'anor',
+#          'apricot':'o\'rik',
+#          'watermelon':'tarvuz',
+#          'melon':'qovun',
+#          'cherry':'gilos',
+#          'orange':'apelsin'
+#          }
+# for key,value in sorted(Python.items()):
+#     print(f"{key.title()} uzbek tilida {value.title()} bo'ladi")
+# =============================================================================
+    
+
+# =============================================================================
+# Davlatlar = {
+#             'Uzbekistan':'Tashkent',
+#             'Canada':'Toronto',
+#             'Japan':'Tokio',
+#             'USA':'Washington',
+#             'South Korea':'Seoul',
+#             'Norway':'Oslo'
+#             }
+# for davlat in sorted(Davlatlar.keys()):
+#     print(f"Davlat: {davlat}")
+#     
+# for poytaxt in sorted(Davlatlar.values()):
+#     print(f"Poytaxt: {poytaxt}")
+# =============================================================================
+
+
+# =============================================================================
+# Davlatlar = {
+#             'Uzbekistan':'Tashkent',
+#             'Canada':'Toronto',
+#             'Japan':'Tokio',
+#             'USA':'Washington',
+#             'South Korea':'Seoul',
+#             'Norway':'Oslo'
+#             }
+# 
+# davlat = input(f"Istalgan davlat nomini kiriting: ")
+# if davlat in Davlatlar:
+#     print(f"{davlat} poytaxti {Davlatlar[davlat]}")
+# else:
+#     print("Bizda bunday ma'lumot yo'q")
+# =============================================================================
+
+
+# =============================================================================
+# menyu = {
+#         'osh':'22000',
+#         'manti':'7000',
+#         'qiyma shashlik':'12500',
+#         'jaz shashlik':'15000',
+#         'tovuq shashlik':'11000',
+#         'qaynatma':'23000',
+#         'non':'5000',
+#         'kompot':'11000',
+#         'bishteks':'30000',
+#         'somsa':'8000'
+#         } 
+# 
+# orders = []
+# taom_1 = input(f"1-taomni kiriting: ") 
+# taom_2 = input(f"2-taomni kiriting: ") 
+# taom_3 = input(f"3-taomni kiriting: ") 
+# orders.append(taom_1)
+# orders.append(taom_2)
+# orders.append(taom_3)
+# for taom in menyu:
+#     if taom in orders:
+#         print(f"{taom} {menyu[taom]} so'm.")
+#     else:
+#         print("Bizda bunday taom yo'q")
+# =============================================================================
+         
+   
---
+++ lesson_(5.3).py
@@ -0,0 +1,63 @@
+# =============================================================================
+# colors = {'yellow', 'pink', 'black'}
+# =============================================================================
+
+
+# =============================================================================
+# colors = {'yellow', 'pink', 'black'}
+# colors.add('white')
+# colors.update(['red', 'green'])
+# =============================================================================
+
+
+# =============================================================================
+# set1 = {10, 20, 30, 40, 50}
+# set2 = {30, 40, 50, 60, 70}
+# 
+# set3 = (set1 & set2) #or (set1.intersection(set2))
+# print(set3)
+# =============================================================================
+
+
+# =============================================================================
+# set1 = {10, 20, 30, 40, 50}
+# set2 = {30, 40, 50, 60, 70}
+# 
+# set3 = set1 - set2 # set2 ning set1 dan farqi 
+# #or set3 = set1.difference(set2)
+# print(set3)
+# 
+# set4 = set2 - set1 # set1 ning set2 dan farqi
+# #or set3 = set2.difference(set1)
+# print(set4)
+# =============================================================================
+
+
+# =============================================================================
+# set1 = {10, 20, 30, 40, 50}
+# set2 = {30, 40, 50, 60, 70}
+# 
+# set3 = set1 ^ set2 #simmetrik farq  
+# #or set3 = set1.symmetric_difference(set2) 
+# print(set3)
+# =============================================================================
+
+
+# =============================================================================
+# bozorlik = ['choy','non','kartoshka','tuxum','sut']
+# mahsulotlar = ['non','sut','tuxum','olma','un','tuz']
+# 
+# mavjud_mahsulotlar = []
+# mavjud_bolmagan = []
+# new_mahsulotlar = []
+# for m in bozorlik:
+#     if m in mahsulotlar:
+#         mavjud_mahsulotlar.append(m)
+#     else:
+#         mavjud_bolmagan.append(m)
+# print(f"mavjud mahsulotlar: {mavjud_mahsulotlar}")
+# print(f"mavjud bo'lmagan mahsulotlar: {mavjud_bolmagan}")
+# for n in mavjud_bolmagan:
+#     new_mahsulotlar.append(n)
+# print(f"yangi mahsulotlar{new_mahsulotlar}")
+# =============================================================================
